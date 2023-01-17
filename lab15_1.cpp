a6da3d02dcf636297022364c99c47be899a991bb
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-01-17 17:33:11 +0700

    I feel rough so much ;_;

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	T keep;
+	for(int i = 1; i < N; i++){
+		int j = i -1;	
+		keep = d[i];
+		while (keep > d[j] && j >= 0 )
+		{
+			d[j+1] = d[j];
+			j = j -1;
+		}
+		d[j+1] = keep;
+	cout << "Pass "<< i <<":";
+	for(int a = 0; a<N ;a++){
+		cout << d[a]<<" ";
+	}
+	cout << "\n";
+	}
 }
+
+
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +35,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
